8ab3b9c0782e2ba00fc9122fe03b53ae7a170733
sblandford <simon@bkconnect.net>
2018-05-05 08:25:21 +0100

    Fetch stat thread started

--- app/src/main/java/eu/bkwsu/webcast/wifitranslation/HubComms.java
+++ app/src/main/java/eu/bkwsu/webcast/wifitranslation/HubComms.java
@@ -2,19 +2,30 @@ package eu.bkwsu.webcast.wifitranslation;
 
 import android.util.Log;
 
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.HttpURLConnection;
 import java.net.InetAddress;
+import java.net.URL;
 import java.net.UnknownHostException;
 import java.nio.charset.Charset;
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class HubComms {
     private static final String TAG = "HubComms";
 
     private static boolean hubIpRun = false;
     private static boolean hubPollRun = false;
+    private static boolean hubByHostname = false;
     private static String hubIp = "";
 
     private static int HUB_BROADCAST_LENGTH_MAX;
@@ -22,6 +33,8 @@ public class HubComms {
     private static int HUB_BROADCAST_PORT;
     private static String HUB_HOSTNAME;
     private static int HUB_POLL_ATTEMPTS;
+    private static int HUB_POLL_INTERVAL_MILLISECONDS;
+    private static String HUB_PROTOCOL;
     private static String HUB_STAT_PATH;
     private static int HUB_STAT_PORT;
     private static int PACKET_BUFFER_SIZE;
@@ -32,6 +45,8 @@ public class HubComms {
         HUB_BROADCAST_PORT = Integer.parseInt(prop.getProperty("HUB_BROADCAST_PORT"));
         HUB_HOSTNAME = prop.getProperty("HUB_HOSTNAME");
         HUB_POLL_ATTEMPTS = Integer.parseInt(prop.getProperty("HUB_POLL_ATTEMPTS"));
+        HUB_POLL_INTERVAL_MILLISECONDS = Integer.parseInt(prop.getProperty("HUB_POLL_INTERVAL_MILLISECONDS"));
+        HUB_PROTOCOL = prop.getProperty("HUB_PROTOCOL");
         HUB_STAT_PATH = prop.getProperty("HUB_STAT_PATH");
         HUB_STAT_PORT = Integer.parseInt(prop.getProperty("HUB_STAT_PORT"));
         PACKET_BUFFER_SIZE = Integer.parseInt(prop.getProperty("RX_PACKET_BUFFER_SIZE"));
@@ -50,14 +65,17 @@ public class HubComms {
                 try {
                     byte[] packetBuff = new byte[PACKET_BUFFER_SIZE];
 
-                    try {
-                        InetAddress address = InetAddress.getByName(HUB_HOSTNAME);
-                        hubIp = address.getHostAddress();
-                        Log.i(TAG, "Hub IP address detected by name resolution : " + hubIp);
-                        hubIpRun = false;
-                        pollHubStart ();
-                    } catch (UnknownHostException e) {
-                        Log.i(TAG, "Unable to resolve Hub by name : " + HUB_HOSTNAME);
+                    if (!hubByHostname) {
+                        try {
+                            InetAddress address = InetAddress.getByName(HUB_HOSTNAME);
+                            hubIp = address.getHostAddress();
+                            Log.i(TAG, "Hub IP address detected by name resolution : " + hubIp);
+                            hubIpRun = false;
+                            hubByHostname = true;
+                            pollHubStart();
+                        } catch (UnknownHostException e) {
+                            Log.i(TAG, "Unable to resolve Hub by name : " + HUB_HOSTNAME);
+                        }
                     }
 
                     //Keep a socket open to listen to all the UDP trafic that is destined for this port
@@ -118,8 +136,55 @@ public class HubComms {
             @Override
             public void run() {
                 int pollCount = HUB_POLL_ATTEMPTS;
+                HttpURLConnection urlConnection = null;
+                
                 while (hubPollRun && (pollCount > 0)) {
-                    hubPollRun = false;
+
+                    StringBuilder result = new StringBuilder();
+
+                    try {
+                        URL url = new URL(((hubByHostname)?HUB_PROTOCOL + "://" + HUB_HOSTNAME:"http://" + hubIp)+ ":" + HUB_STAT_PORT + "/" + HUB_STAT_PATH);
+                        urlConnection = (HttpURLConnection) url.openConnection();
+                        InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+                            result.append(line);
+                        }
+                        pollCount = HUB_POLL_ATTEMPTS;
+
+                        Pattern pattern = Pattern.compile("\\((.*)\\)");
+                        Matcher matcher = pattern.matcher(result);
+                        if (matcher.find())
+                        {
+                            //https://stackoverflow.com/questions/9605913/how-to-parse-json-in-android?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+                            Log.i(TAG, matcher.group(1));
+                            JSONObject jObject = new JSONObject(matcher.group(1));
+                            //JSONObject jObject0 = jObject.getJSONObject("0");
+                            //Log.i(TAG,jObject0.toString());
+                        }
+
+                    }catch( Exception e) {
+                        Log.d(TAG,"Attempt " + (HUB_POLL_ATTEMPTS - pollCount) + ": Failed to read " + HUB_PROTOCOL + "://" + HUB_HOSTNAME + ":" + HUB_STAT_PORT + "/" + HUB_STAT_PATH);
+                        pollCount--;
+                    } finally {
+                        if (urlConnection != null) {
+                            urlConnection.disconnect();
+                        }
+                    }
+
+                    //Sleep for a bit
+                    try {
+                        Thread.sleep(HUB_POLL_INTERVAL_MILLISECONDS);
+                    } catch (InterruptedException e) {
+                        Log.d(TAG, "Active state thread interrupted");
+                    }
+                }
+                //If we hostname-based fetch failed then try looking for broadcast of IP instead
+                if ((pollCount == 0) && hubByHostname) {
+                    broadcastStart ();
                 }
             }
         };
